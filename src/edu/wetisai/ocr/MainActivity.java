a865c8edea3eef13b140950108eb97c0d51c2b8f
weitsai <begining1003@gmail.com>
2014-10-05 00:06:16 +0800

    把進入點改 SampleList

--- src/edu/wetisai/ocr/MainActivity.java
+++ src/edu/wetisai/ocr/MainActivity.java
@@ -1,93 +1,39 @@
 
 package edu.wetisai.ocr;
 
-import com.googlecode.tesseract.android.TessBaseAPI;
-
-import android.graphics.Bitmap;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.Paint.Align;
+import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.ImageView;
-import android.widget.TextView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class MainActivity extends ActionBarActivity {
-    private ImageView image;
-    private TextView tessResults;
-    private Bitmap mTextBitmap;
-    private TessBaseAPI baseApi;
+public class MainActivity extends ListActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        findView();
-
-        mTextBitmap = getTextImage("你好", 300, 300);
-        image.setImageBitmap(mTextBitmap);
-
-        baseApi = new TessBaseAPI();
-        baseApi.init("/mnt/sdcard/tesseract/", "chi_tra");
-        baseApi.setPageSegMode(TessBaseAPI.PageSegMode.PSM_SINGLE_LINE);
-        baseApi.setImage(mTextBitmap);
-        String outputText = baseApi.getUTF8Text();
-        tessResults.setText(outputText);
+        setListAdapter(ArrayAdapter.createFromResource(this, R.array.sampe_list, android.R.layout.simple_list_item_1));
     }
 
     @Override
-    protected void onDestroy() {
-        mTextBitmap.recycle();
-        baseApi.end();
-        super.onDestroy();
-    }
-
-    private void findView() {
-        image = findViewId(R.id.image);
-        tessResults = findViewId(R.id.results);
-    }
-
-    private <T extends View> T findViewId(int resId) {
-        return (T) findViewById(resId);
-    }
-
-    private Bitmap getTextImage(String text, int width, int height) {
-        final Bitmap bmp = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
-        final Paint paint = new Paint();
-        final Canvas canvas = new Canvas(bmp);
-
-        canvas.drawColor(Color.WHITE);
-
-        paint.setColor(Color.BLACK);
-        paint.setAntiAlias(true);
-        paint.setTextAlign(Align.CENTER);
-        paint.setTextSize(24.0f);
-        canvas.drawText(text, width / 2, height / 2, paint);
-
-        return bmp;
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        Class purpose;
+        
+        switch (position) {
+            case 0:
+                purpose = BitmapORC.class;
+                break;
+                
+            case 1:
+                purpose = CameraORC.class;
+                break;
+                
+            default:
+                return;
         }
-        return super.onOptionsItemSelected(item);
+        
+        Intent intent = new Intent(this, purpose);
+        startActivity(intent);
     }
-
 }
